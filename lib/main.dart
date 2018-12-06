c5eac757c7096f092f634539aeac75073e39b0d3
Tom Fitzpatrick <tfitz237@gmail.com>
2018-12-05 22:42:24 -0500

    feat: bottomSheet test

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,10 @@ class ActivityItem {
 
 
 class CriteriaState extends State<Criterias> {
+
+
+  final _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   List<ActivityItem> items = <ActivityItem>[
      ActivityItem(
         false,
@@ -52,6 +56,54 @@ class CriteriaState extends State<Criterias> {
     )
     //give all your items here
   ];
+
+  VoidCallback _showBottomSheetCallback;
+
+  PersistentBottomSheetController _bottomSheetController;
+  @override
+  void initState() {
+    super.initState();
+    _showBottomSheetCallback = _showBottomSheet;
+  }
+
+  void _toggleBottomSheet() {
+    if (_showBottomSheetCallback == null) {
+      _bottomSheetController.close();
+    } else {
+      _showBottomSheetCallback();
+    }
+  }
+
+  void _showBottomSheet() {
+    setState(() { // disable the button
+      _showBottomSheetCallback = null;
+    });
+    _bottomSheetController = _scaffoldKey.currentState.showBottomSheet((BuildContext context) {
+      final ThemeData themeData = Theme.of(context);
+      return Container(
+          decoration: BoxDecoration(
+              border: Border(top: BorderSide(color: themeData.disabledColor))
+          ),
+          child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Text('This is a Material persistent bottom sheet. Drag downwards to dismiss it.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: themeData.accentColor,
+                      fontSize: 24.0
+                  )
+              )
+          )
+      );
+    });
+    _bottomSheetController.closed.whenComplete(() {
+      if (mounted) {
+        setState(() { // re-enable the button
+          _showBottomSheetCallback = _showBottomSheet;
+        });
+      }
+    });
+  }
 
   ListView List_Criteria;
   Widget build(BuildContext context) {
@@ -95,32 +147,28 @@ class CriteriaState extends State<Criterias> {
     );
 
     Scaffold scaffold =  Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Timehedron"),
         actions: <Widget>[
-          PopupMenuButton(itemBuilder: (BuildContext context) =>
-            <PopupMenuEntry>[
-              PopupMenuItem(child: Text('Settings'))
-            ]
-            )
+          PopupMenuButton<int>(itemBuilder: (BuildContext context) =>
+            <PopupMenuEntry<int>>[
+              PopupMenuItem<int>(child: Text('Settings'), value: 0)
+            ],
+            onSelected: (int result) {
+              setState(() {
+              });
+            })
 
         ]
 
       ),
       body: List_Criteria,
-//      persistentFooterButtons: <Widget>[
-//         ButtonBar(children: <Widget>[
-//           FlatButton(
-//            color: Colors.blue,
-//            onPressed: null,
-//            child:  Text(
-//              'Apply',
-//              textAlign: TextAlign.left,
-//              style:  TextStyle(fontWeight: FontWeight.bold),
-//            ),
-//          )
-//        ])
-//      ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleBottomSheet,
+        child: _showBottomSheetCallback != null ? Icon(Icons.add) : Icon(Icons.remove),
+        backgroundColor: _showBottomSheetCallback != null ? Colors.blueGrey : Colors.grey,
+      ),
     );
     return MaterialApp(home: scaffold, theme:  ThemeData.dark());
   }
